97087f6ad3d200055b1988fea2859d653eee48a7
Certil Remy <certil@noukod.com>
2019-01-14 20:36:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
   user = User.new(:username => params[:username], :password => params[:password])
- 
+
   if user.save
     redirect "/login"
   else
@@ -32,6 +32,11 @@ end
 
   post "/login" do
   user = User.find_by(:username => params[:username])
+  if user
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
 end
 
 	get "/success" do
